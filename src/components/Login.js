5a41b569ccad5e860ef737948d4a5e5d034d79a0
Brajesh Kumar <brajeshk922@gmail.com>
2021-08-12 12:48:03 +0530

    credentials same-origin

--- src/components/Login.js
+++ src/components/Login.js
@@ -29,7 +29,7 @@ const Login = () => {
                 'Content-Type': "application/json"
             },
             body: JSON.stringify({ email, password }),
-            // credentials: "include"
+            credentials: "include"
         });
 
         const data = await res.json();
